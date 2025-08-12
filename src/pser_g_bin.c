e77f56ee0292f3f32b561a242926db54c79a40f8
sergeev.ad <sergeev.ad@inbres.ru>
2025-08-12 15:42:47 +0300

    div and mul for numbers

--- src/pser_g_bin.c
+++ src/pser_g_bin.c
@@ -137,6 +137,49 @@ struct GlobExpr *glob_sub_int_and_real(struct GlobExpr *l, struct GlobExpr *r) {
 	return l;
 }
 // ###########################################################################################
+// 											*
+// ###########################################################################################
+struct GlobExpr *glob_mul_two_ints(struct GlobExpr *l, struct GlobExpr *r) {
+	l->tvar->num *= r->tvar->num;
+	return l;
+}
+struct GlobExpr *glob_mul_two_reals(struct GlobExpr *l, struct GlobExpr *r) {
+	l->tvar->real *= r->tvar->real;
+	return l;
+}
+struct GlobExpr *glob_mul_real_and_int(struct GlobExpr *l, struct GlobExpr *r) {
+	l->tvar->real *= r->tvar->num;
+	return l;
+}
+struct GlobExpr *glob_mul_int_and_real(struct GlobExpr *l, struct GlobExpr *r) {
+	l->tvar->real = l->tvar->num * l->tvar->real;
+	l->code = r->code;
+	return l;
+}
+// ###########################################################################################
+// 											/
+// ###########################################################################################
+#define is_int_zero(e) ( (e)->tvar->num == 0 )
+#define is_real_zero(e) ( (e)->tvar->real == 0.0 )
+
+struct GlobExpr *glob_div_two_ints(struct GlobExpr *l, struct GlobExpr *r) {
+	l->tvar->num /= r->tvar->num;
+	return l;
+}
+struct GlobExpr *glob_div_two_reals(struct GlobExpr *l, struct GlobExpr *r) {
+	l->tvar->real /= r->tvar->real;
+	return l;
+}
+struct GlobExpr *glob_div_real_and_int(struct GlobExpr *l, struct GlobExpr *r) {
+	l->tvar->real /= r->tvar->num;
+	return l;
+}
+struct GlobExpr *glob_div_int_and_real(struct GlobExpr *l, struct GlobExpr *r) {
+	l->tvar->real = l->tvar->num / l->tvar->real;
+	l->code = r->code;
+	return l;
+}
+// ###########################################################################################
 //
 // ###########################################################################################
 
@@ -173,24 +216,41 @@ struct GlobExpr *global_bin(struct Pser *p, struct GlobExpr *l,
 		else
 			exit(220); // just for now later do it not as a черт
 	} else if (op->code == MINUS) {
-		if (l->code == CT_INT && r->code == CT_INT)
+		if (is_ct_int(l) && is_ct_int(r))
 			l = glob_sub_two_ints(l, r);
-		else if (l->code == CT_REAL && r->code == CT_REAL)
+		else if (is_ct_real(l) && is_ct_real(r))
 			l = glob_sub_two_reals(l, r);
-		else if (l->code == CT_REAL && r->code == CT_INT)
+		else if (is_ct_real(l) && is_ct_int(r))
 			l = glob_sub_real_and_int(l, r);
-		else if (l->code == CT_INT && r->code == CT_REAL)
+		else if (is_ct_int(l) && is_ct_real(r))
 			l = glob_sub_int_and_real(l, r);
 		else
 			exit(219);
 	} else if (op->code == MUL) {
-		if (l->code == CT_INT && r->code == CT_INT)
+		if (is_ct_int(l) && is_ct_int(r))
 			l = glob_mul_two_ints(l, r);
+		else if (is_ct_real(l) && is_ct_real(r))
+			l = glob_mul_two_reals(l, r);
+		else if (is_ct_real(l) && is_ct_int(r))
+			l = glob_mul_real_and_int(l, r);
+		else if (is_ct_int(l) && is_ct_real(r))
+			l = glob_mul_int_and_real(l, r);
 		else
 			exit(218);
 	} else if (op->code == DIV) {
-		if (l->code == CT_INT && r->code == CT_INT)
+		if (is_ct_int(r) && is_int_zero(r))
+			exit(216);
+		if (is_ct_real(r) && is_rael_zero(r))
+			exit(215);
+		
+		if (is_ct_int(l) && is_ct_int(r))
 			l = glob_div_two_ints(l, r);
+		else if (is_ct_real(l) && is_ct_real(r))
+			l = glob_div_two_reals(l, r);
+		else if (is_ct_real(l) && is_ct_int(r))
+			l = glob_div_real_and_int(l, r);
+		else if (is_ct_int(l) && is_ct_real(r))
+			l = glob_div_int_and_real(l, r);
 		else
 			exit(217);
 	}
