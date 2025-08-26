5b6bd1a89c2927746ebdd11d548dfdfc05d65b3b
chunchunmaru0000 <cho227675m@gmail.com>
2025-08-26 06:43:44 +0300

    can just remove macro call from tokens, not yet apply it

--- src/prep_se.c
+++ src/prep_se.c
@@ -9,14 +9,17 @@ const char *const PAR_CUR_WASNT_CLOSED = "'{' не была закрыта.";
 const char *const CLOSE_PAR_CUR = "закрыть '{'";
 const char *const NOTHING_TO_CLOSE =
 	"Встречена закрывающая скобка, но не было открывающей.";
+const char *const CANT_HAVE_EMPTY_ARG_YET =
+	"TODO: Аргумент не может быть пустым.";
 
 struct BList *last_opened_stack = 0;
 
 void exit_par(struct Token *token) {
-	enum TCode last_closed = b_last(last_opened_stack);
-
 	if (last_opened_stack->size <= 0)
 		eet(token, NOTHING_TO_CLOSE, 0);
+
+	enum TCode last_closed = b_last(last_opened_stack);
+
 	if (last_closed == PAR_L && token->code != PAR_R)
 		eet(token, PAR_WASNT_CLOSED, CLOSE_PAR);
 	if (last_closed == PAR_C_L && token->code != PAR_C_R)
@@ -31,11 +34,18 @@ void exit_par(struct Token *token) {
 // while in macro body, so no need to do it twice
 struct Nodes *parse_macro_arg_nodes(struct NodeToken **c) {
 	struct Nodes *arg_nodes = malloc(sizeof(struct Nodes));
-	enum TCode code;
+	enum TCode code = (*c)->token->code;
+
+	if (code == PAR_R || code == COMMA)
+		eet((*c)->token, CANT_HAVE_EMPTY_ARG_YET, 0);
+
+	if (!last_opened_stack)
+		last_opened_stack = new_blist(16);
+	else
+		last_opened_stack->size = 0;
 
 	arg_nodes->fst = *c;
 
-	(*c) = take_guaranteed_next(*c);
 	for (;; (*c) = take_guaranteed_next(*c)) {
 		code = (*c)->token->code;
 
@@ -49,45 +59,40 @@ struct Nodes *parse_macro_arg_nodes(struct NodeToken **c) {
 	}
 
 	arg_nodes->lst = (*c)->prev; // prev cuz c is or PAR_CLOSE or COMMA
+
+	(*c) = take_guaranteed_next(*c); // skip PAR_CLOSE or COMMA
 	return arg_nodes;
 }
 
-const char *const EXPECTED_PAR_R_OR_COMMA_AFTER_MACRO_ARGS =
-	"Ожидалась ')' или ',' после аргументов макро.";
-
-struct PList *parse_macro_args_nodes(struct NodeToken *c, struct Macro *macro) {
+struct PList *parse_macro_args_nodes(struct NodeToken **c,
+									 struct Macro *macro) {
 	struct PList *args_nodes = new_plist(macro->args->size);
 	uint32_t i;
 
 	printf("asdf\n");
 
-	c = take_guaranteed_next(c); // here its takes one after macro name
+	(*c) = take_guaranteed_next(*c); // here its takes one after macro name
 
 	// if not parentices then why not just begin args
-	if (c->token->code == PAR_L)
-		c = take_guaranteed_next(c);
+	if ((*c)->token->code == PAR_L)
+		*c = take_guaranteed_next(*c);
 
 	for (i = 0; i < macro->args->size; i++)
-		plist_add(args_nodes, parse_macro_arg_nodes(&c));
-
-	if (c->token->code != PAR_R && c->token->code != COMMA)
-		eet(c->token, EXPECTED_PAR_R_OR_COMMA_AFTER_MACRO_ARGS, 0);
+		plist_add(args_nodes, parse_macro_arg_nodes(c));
 
 	return args_nodes;
 }
 
 struct NodeToken *call_macro(struct NodeToken *c, struct Macro *macro) {
-	struct NodeToken *fst_to_cut = c, *lst_to_cut, *head;
+	struct NodeToken *fst_to_cut = c;
+
 	// all of args_nodes nodes need to clone when insert
-	struct PList *args_nodes = parse_macro_args_nodes(c, macro);
+	struct PList *args_nodes = parse_macro_args_nodes(&c, macro);
 
 	// here apply args nodes to args usages
 
-	if (fst_to_cut->prev)
-		fst_to_cut->prev->next = head;
-	head->prev = fst_to_cut;
-
-	return head;
+	c = cut_off_inclusive(fst_to_cut, c->prev);
+	return c;
 }
 
 // ####################################################################
