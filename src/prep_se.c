62fc51ed4101485e461fdb4a7959fbdf0ac26d1a
chunchunmaru0000 <cho227675m@gmail.com>
2025-08-26 07:17:58 +0300

    more consistent way of closing args

--- src/prep_se.c
+++ src/prep_se.c
@@ -11,6 +11,8 @@ const char *const NOTHING_TO_CLOSE =
 	"Встречена закрывающая скобка, но не было открывающей.";
 const char *const CANT_HAVE_EMPTY_ARG_YET =
 	"TODO: Аргумент не может быть пустым.";
+const char *const NEED_CLOSE_BY_PAR_R = "Аргумент должен был быть закрыт ')'.";
+const char *const NEED_CLOSE_BY_COMMA = "Аргумент должен был быть закрыт ','.";
 
 struct BList *last_opened_stack = 0;
 
@@ -30,13 +32,14 @@ void exit_par(struct Token *token) {
 	last_opened_stack->size--;
 }
 
+enum TCode close_code;
 // here all token can be just guaranteed cuz they will be applyed
 // while in macro body, so no need to do it twice
 struct Nodes *parse_macro_arg_nodes(struct NodeToken **c) {
 	struct Nodes *arg_nodes = malloc(sizeof(struct Nodes));
 	enum TCode code = (*c)->token->code;
 
-	if (code == PAR_R || code == COMMA)
+	if (code == close_code)
 		eet((*c)->token, CANT_HAVE_EMPTY_ARG_YET, 0);
 
 	if (!last_opened_stack)
@@ -49,8 +52,14 @@ struct Nodes *parse_macro_arg_nodes(struct NodeToken **c) {
 	for (;; (*c) = take_guaranteed_next(*c)) {
 		code = (*c)->token->code;
 
-		if ((code == PAR_R || code == COMMA) && last_opened_stack->size == 0)
-			break;
+		if (last_opened_stack->size == 0) {
+			if (code == close_code)
+				break;
+			if (code == COMMA && close_code == PAR_R)
+				eet((*c)->token, NEED_CLOSE_BY_PAR_R, 0);
+			if (code == PAR_R && close_code == COMMA)
+				eet((*c)->token, NEED_CLOSE_BY_COMMA, 0);
+		}
 
 		if (code == PAR_L || code == PAR_C_L || code == PAR_T_L)
 			blist_add(last_opened_stack, code);
@@ -58,27 +67,52 @@ struct Nodes *parse_macro_arg_nodes(struct NodeToken **c) {
 			exit_par((*c)->token);
 	}
 
-	arg_nodes->lst = (*c)->prev; // prev cuz c is or PAR_CLOSE or COMMA
+	arg_nodes->lst = (*c)->prev; // prev cuz c is close_code
 
-	(*c) = take_guaranteed_next(*c); // skip PAR_CLOSE or COMMA
+	(*c) = take_guaranteed_next(*c); // skip close_code
 	return arg_nodes;
 }
+
+const char *const EXPECTED_PAR_R_AFTER_MACRO_ARGS =
+	"Ожидалась ')' после аргументов макро без аргументов.";
+const char *const EXPECTED_COMMA_AFTER_MACRO_ARGS =
+	"Ожидалась ',' после аргументов макро без аргументов.";
 
 struct PList *parse_macro_args_nodes(struct NodeToken **c,
 									 struct Macro *macro) {
 	struct PList *args_nodes = new_plist(macro->args->size);
 	uint32_t i;
 
-	printf("asdf\n");
-
 	(*c) = take_guaranteed_next(*c); // here its takes one after macro name
 
 	// if not parentices then why not just begin args
-	if ((*c)->token->code == PAR_L)
+	if ((*c)->token->code == PAR_L) {
 		*c = take_guaranteed_next(*c);
+		close_code = PAR_R;
+	} else
+		close_code = COMMA;
 
-	for (i = 0; i < macro->args->size; i++)
-		plist_add(args_nodes, parse_macro_arg_nodes(c));
+	if (macro->args->size) {
+		if (close_code == PAR_R) {
+			// close all args before last with ','
+			close_code = COMMA;
+			for (i = 0; i < macro->args->size - 1; i++)
+				plist_add(args_nodes, parse_macro_arg_nodes(c));
+			// for last arg close with ')'
+			close_code = PAR_R;
+			plist_add(args_nodes, parse_macro_arg_nodes(c));
+		} else {
+			// close all with ','
+			for (i = 0; i < macro->args->size; i++)
+				plist_add(args_nodes, parse_macro_arg_nodes(c));
+		}
+	} else {
+		if (close_code == PAR_R && ((*c)->token->code != PAR_R))
+			eet((*c)->token, EXPECTED_PAR_R_AFTER_MACRO_ARGS, 0);
+		if (close_code == COMMA && (*c)->token->code != COMMA)
+			eet((*c)->token, EXPECTED_COMMA_AFTER_MACRO_ARGS, 0);
+		*c = take_guaranteed_next(*c);
+	}
 
 	return args_nodes;
 }
